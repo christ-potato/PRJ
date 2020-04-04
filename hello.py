ca37e4a5d34955cc596992e576d05719f4b4d5d9
王鹏 <846632090@qq.com>
2020-04-04 22:30:27 +0800

    hello,world

---
+++ hello.py
@@ -0,0 +1,11 @@
+# coding = utf-8
+import time
+class Born:
+    print("hello world")
+    born_time=time.time()
+    print("Now TIME is {},I am borned".format(born_time))
+
+
+
+if __name__ == "__main__":
+    Born()
